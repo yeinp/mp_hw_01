1a4c8d3d2db034bff2e01536e1041f9f83021b1b
Github <Github>
2022-09-22 00:28:35 +0900

    upload

--- app/src/main/java/com/example/hw1_2_20211336/MainActivity.java
+++ app/src/main/java/com/example/hw1_2_20211336/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
